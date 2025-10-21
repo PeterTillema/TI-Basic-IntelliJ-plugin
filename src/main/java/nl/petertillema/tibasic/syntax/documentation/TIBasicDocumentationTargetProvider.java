bbfffca1de48d33a82683b583f2fc84b57cd88f7
Peter Tillema <ptillema@quintor.nl>
2025-10-21 17:45:35 +0200

    Limit documentation to only commands and expression functions

--- src/main/java/nl/petertillema/tibasic/syntax/documentation/TIBasicDocumentationTargetProvider.java
+++ src/main/java/nl/petertillema/tibasic/syntax/documentation/TIBasicDocumentationTargetProvider.java
@@ -3,12 +3,21 @@ package nl.petertillema.tibasic.syntax.documentation;
 import com.intellij.platform.backend.documentation.DocumentationTarget;
 import com.intellij.platform.backend.documentation.DocumentationTargetProvider;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
 import nl.petertillema.tibasic.language.TIBasicFile;
+import nl.petertillema.tibasic.psi.TIBasicTypes;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public final class TIBasicDocumentationTargetProvider implements DocumentationTargetProvider {
+
+    private static final List<IElementType> ALLOWED_DOCUMENTATION_TYPES = List.of(
+            TIBasicTypes.EXPR_FUNCTIONS_WITH_ARGS,
+            TIBasicTypes.EXPR_FUNCTIONS_NO_ARGS,
+            TIBasicTypes.COMMAND_WITH_PARENS,
+            TIBasicTypes.COMMAND_NO_PARENS
+    );
 
     @Override
     public @NotNull List<? extends @NotNull DocumentationTarget> documentationTargets(@NotNull PsiFile file, int offset) {
@@ -19,6 +28,8 @@ public final class TIBasicDocumentationTargetProvider implements DocumentationTa
         }
         var element = file.findElementAt(offset);
         if (element == null) return List.of();
+
+        if (!ALLOWED_DOCUMENTATION_TYPES.contains(element.getNode().getElementType())) return List.of();
 
         return List.of(new TIBasicDocumentationTarget(element));
     }
