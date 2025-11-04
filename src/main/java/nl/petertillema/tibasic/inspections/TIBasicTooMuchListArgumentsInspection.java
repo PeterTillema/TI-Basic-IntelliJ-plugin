abcd7bb6fe9b0f86500070f72f79007f2e4fcda9
Peter Tillema <ptillema@quintor.nl>
2025-11-04 11:56:19 +0100

    Add inspection to report too much arguments for anonymous lists

---
+++ src/main/java/nl/petertillema/tibasic/inspections/TIBasicTooMuchListArgumentsInspection.java
@@ -0,0 +1,29 @@
+package nl.petertillema.tibasic.inspections;
+
+import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElementVisitor;
+import nl.petertillema.tibasic.TIBasicMessageBundle;
+import nl.petertillema.tibasic.psi.TIBasicAnonymousList;
+import nl.petertillema.tibasic.psi.TIBasicVisitor;
+import org.jetbrains.annotations.NotNull;
+
+public final class TIBasicTooMuchListArgumentsInspection extends LocalInspectionTool {
+
+    private static final String ERROR_MESSAGE = TIBasicMessageBundle.message("inspection.TIBasic.too.much.arguments.display.description");
+
+    @Override
+    public @NotNull PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
+        return new TIBasicVisitor() {
+            @Override
+            public void visitAnonymousList(@NotNull TIBasicAnonymousList o) {
+                super.visitAnonymousList(o);
+                if (o.getExprList().size() > 999) {
+                    holder.registerProblem(o, ERROR_MESSAGE, ProblemHighlightType.WARNING);
+                }
+            }
+        };
+    }
+
+}
