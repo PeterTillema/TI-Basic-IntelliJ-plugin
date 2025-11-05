9248bc9b62749d2378f7bb351e2833d40579ff39
Peter Tillema <ptillema@quintor.nl>
2025-11-05 10:50:53 +0100

    Improve constant expression inspection by not storing PsiElement's in the fix and improving the evaluator logic

--- src/main/java/nl/petertillema/tibasic/inspections/codeStyle/TIBasicConstantExpressionInspection.java
+++ src/main/java/nl/petertillema/tibasic/inspections/codeStyle/TIBasicConstantExpressionInspection.java
@@ -13,7 +13,6 @@ import nl.petertillema.tibasic.TIBasicMessageBundle;
 import nl.petertillema.tibasic.psi.TIBasicAndExpr;
 import nl.petertillema.tibasic.psi.TIBasicDivExpr;
 import nl.petertillema.tibasic.psi.TIBasicEqExpr;
-import nl.petertillema.tibasic.psi.TIBasicExpr;
 import nl.petertillema.tibasic.psi.TIBasicGeExpr;
 import nl.petertillema.tibasic.psi.TIBasicGtExpr;
 import nl.petertillema.tibasic.psi.TIBasicLeExpr;
@@ -30,8 +29,9 @@ import nl.petertillema.tibasic.psi.TIBasicXorExpr;
 import org.jetbrains.annotations.NotNull;
 
 import java.math.BigDecimal;
-import java.util.List;
-import java.util.function.BinaryOperator;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
 import static nl.petertillema.tibasic.psi.TIBasicUtil.createFromText;
 
@@ -45,114 +45,94 @@ public final class TIBasicConstantExpressionInspection extends LocalInspectionTo
             @Override
             public void visitPlusExpr(@NotNull TIBasicPlusExpr o) {
                 super.visitPlusExpr(o);
-                this.checkOperator(o, o.getExprList(), BigDecimal::add);
+                this.checkOperator(o);
             }
 
             @Override
             public void visitMinusExpr(@NotNull TIBasicMinusExpr o) {
                 super.visitMinusExpr(o);
-                this.checkOperator(o, o.getExprList(), BigDecimal::subtract);
+                this.checkOperator(o);
             }
 
             @Override
             public void visitMulExpr(@NotNull TIBasicMulExpr o) {
                 super.visitMulExpr(o);
-                this.checkOperator(o, o.getExprList(), BigDecimal::multiply);
+                this.checkOperator(o);
             }
 
             @Override
             public void visitDivExpr(@NotNull TIBasicDivExpr o) {
                 super.visitDivExpr(o);
-                this.checkOperator(o, o.getExprList(), BigDecimal::divide);
+                this.checkOperator(o);
             }
 
             @Override
             public void visitEqExpr(@NotNull TIBasicEqExpr o) {
                 super.visitEqExpr(o);
-                this.checkOperator(o, o.getExprList(), (num1, num2) ->
-                        num1.compareTo(num2) == 0 ? BigDecimal.ONE : BigDecimal.ZERO);
+                this.checkOperator(o);
             }
 
             @Override
             public void visitNeExpr(@NotNull TIBasicNeExpr o) {
                 super.visitNeExpr(o);
-                this.checkOperator(o, o.getExprList(), (num1, num2) ->
-                        num1.compareTo(num2) != 0 ? BigDecimal.ONE : BigDecimal.ZERO);
+                this.checkOperator(o);
             }
 
             @Override
             public void visitLtExpr(@NotNull TIBasicLtExpr o) {
                 super.visitLtExpr(o);
-                this.checkOperator(o, o.getExprList(), (num1, num2) ->
-                        num1.compareTo(num2) < 0 ? BigDecimal.ONE : BigDecimal.ZERO);
+                this.checkOperator(o);
             }
 
             @Override
             public void visitLeExpr(@NotNull TIBasicLeExpr o) {
                 super.visitLeExpr(o);
-                this.checkOperator(o, o.getExprList(), (num1, num2) ->
-                        num1.compareTo(num2) <= 0 ? BigDecimal.ONE : BigDecimal.ZERO);
+                this.checkOperator(o);
             }
 
             @Override
             public void visitGtExpr(@NotNull TIBasicGtExpr o) {
                 super.visitGtExpr(o);
-                this.checkOperator(o, o.getExprList(), (num1, num2) ->
-                        num1.compareTo(num2) > 0 ? BigDecimal.ONE : BigDecimal.ZERO);
+                this.checkOperator(o);
             }
 
             @Override
             public void visitGeExpr(@NotNull TIBasicGeExpr o) {
                 super.visitGeExpr(o);
-                this.checkOperator(o, o.getExprList(), (num1, num2) ->
-                        num1.compareTo(num2) >= 0 ? BigDecimal.ONE : BigDecimal.ZERO);
+                this.checkOperator(o);
             }
 
             @Override
             public void visitOrExpr(@NotNull TIBasicOrExpr o) {
                 super.visitOrExpr(o);
-                this.checkOperator(o, o.getExprList(), (num1, num2) ->
-                        num1.compareTo(BigDecimal.ZERO) != 0 || num2.compareTo(BigDecimal.ZERO) != 0 ? BigDecimal.ONE : BigDecimal.ZERO);
+                this.checkOperator(o);
             }
 
             @Override
             public void visitXorExpr(@NotNull TIBasicXorExpr o) {
                 super.visitXorExpr(o);
-                this.checkOperator(o, o.getExprList(), (num1, num2) ->
-                        num1.compareTo(BigDecimal.ZERO) != 0 ^ num2.compareTo(BigDecimal.ZERO) != 0 ? BigDecimal.ONE : BigDecimal.ZERO);
+                this.checkOperator(o);
             }
 
             @Override
             public void visitAndExpr(@NotNull TIBasicAndExpr o) {
                 super.visitAndExpr(o);
-                this.checkOperator(o, o.getExprList(), (num1, num2) ->
-                        num1.compareTo(BigDecimal.ZERO) != 0 && num2.compareTo(BigDecimal.ZERO) != 0 ? BigDecimal.ONE : BigDecimal.ZERO);
+                this.checkOperator(o);
             }
 
-            private void checkOperator(PsiElement element, List<TIBasicExpr> expressions, BinaryOperator<BigDecimal> evaluator) {
-                if (expressions.size() != 2) return;
-
-                if (!(expressions.getFirst() instanceof TIBasicLiteralExpr l1)) return;
-                if (!(expressions.getLast() instanceof TIBasicLiteralExpr l2)) return;
+            private void checkOperator(PsiElement element) {
+                if (element.getChildren().length != 2) return;
+                if (!(element.getFirstChild() instanceof TIBasicLiteralExpr l1)) return;
+                if (!(element.getLastChild() instanceof TIBasicLiteralExpr l2)) return;
 
                 if (l1.getFirstChild().getNode().getElementType() == TIBasicTypes.NUMBER && l2.getFirstChild().getNode().getElementType() == TIBasicTypes.NUMBER) {
-                    holder.registerProblem(element, ERROR_MESSAGE, new ReplaceByConstantQuickFix(expressions.getFirst(), expressions.getLast(), evaluator));
+                    holder.registerProblem(element, ERROR_MESSAGE, new ReplaceByConstantQuickFix());
                 }
             }
         };
     }
 
     private static class ReplaceByConstantQuickFix implements LocalQuickFix {
-
-        private final PsiElement first;
-        private final PsiElement second;
-        private final BinaryOperator<BigDecimal> evaluator;
-
-        private ReplaceByConstantQuickFix(PsiElement first, PsiElement second, BinaryOperator<BigDecimal> evaluator) {
-            this.first = first;
-            this.second = second;
-            this.evaluator = evaluator;
-        }
 
         @Override
         public @IntentionFamilyName @NotNull String getFamilyName() {
@@ -161,12 +141,65 @@ public final class TIBasicConstantExpressionInspection extends LocalInspectionTo
 
         @Override
         public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
-            var result = evaluator.apply(new BigDecimal(first.getText()), new BigDecimal(second.getText()));
+            var element = descriptor.getPsiElement();
+            if (element == null) return;
+            var evaluator = getEvaluator(element);
+            if (evaluator == null) return;
+
+            var result = evaluator.apply(new BigDecimal(element.getFirstChild().getText()), new BigDecimal(element.getLastChild().getText()));
             var tempBasicFile = createFromText(project, result.toString());
             var literalExpr = PsiTreeUtil.findChildOfType(tempBasicFile, TIBasicLiteralExpr.class);
             if (literalExpr == null) return;
 
-            descriptor.getPsiElement().replace(literalExpr);
+            element.replace(literalExpr);
+        }
+
+        private BiFunction<BigDecimal, BigDecimal, BigDecimal> getEvaluator(PsiElement element) {
+            return switch (element) {
+                case TIBasicPlusExpr ignored -> BigDecimal::add;
+                case TIBasicMinusExpr ignored -> BigDecimal::subtract;
+                case TIBasicMulExpr ignored -> BigDecimal::multiply;
+                case TIBasicDivExpr ignored -> BigDecimal::divide;
+                case TIBasicEqExpr ignored -> equalityTest(result -> result == 0);
+                case TIBasicNeExpr ignored -> equalityTest(result -> result != 0);
+                case TIBasicLtExpr ignored -> equalityTest(result -> result < 0);
+                case TIBasicLeExpr ignored -> equalityTest(result -> result <= 0);
+                case TIBasicGtExpr ignored -> equalityTest(result -> result > 0);
+                case TIBasicGeExpr ignored -> equalityTest(result -> result >= 0);
+                case TIBasicOrExpr ignored -> logicalTest(Boolean::logicalOr);
+                case TIBasicXorExpr ignored -> logicalTest(Boolean::logicalXor);
+                case TIBasicAndExpr ignored -> logicalTest(Boolean::logicalAnd);
+                default -> null;
+            };
+        }
+
+        /**
+         * Performs an equality test between the two given operands. Based on the result, the output is always a
+         * BigDecimal of 1 or 0.
+         *
+         * @param comparisonFunction The function to test the comparison result against.
+         * @return A BigDecimal of 1 or 0, based on whether the comparison function was true.
+         */
+        private BiFunction<BigDecimal, BigDecimal, BigDecimal> equalityTest(Predicate<Integer> comparisonFunction) {
+            BiFunction<BigDecimal, BigDecimal, Integer> func = BigDecimal::compareTo;
+            return func.andThen(x -> comparisonFunction.test(x) ? BigDecimal.ONE : BigDecimal.ZERO);
+        }
+
+        /**
+         * Performs a logical test between the two given operands. Based on the result, the output is always a
+         * BigDecimal of 1 or 0.
+         *
+         * @param logicalTest The function to test the comparison result against.
+         * @return A BigDecimal of 1 or 0, based on whether the comparison function was true.
+         */
+        private BiFunction<BigDecimal, BigDecimal, BigDecimal> logicalTest(BiPredicate<Boolean, Boolean> logicalTest) {
+            Predicate<BigDecimal> nonZeroPredicate = num1 -> num1.compareTo(BigDecimal.ZERO) != 0;
+
+            return (num1, num2) -> {
+                var result1 = nonZeroPredicate.test(num1);
+                var result2 = nonZeroPredicate.test(num2);
+                return logicalTest.test(result1, result2) ? BigDecimal.ONE : BigDecimal.ZERO;
+            };
         }
     }
 
