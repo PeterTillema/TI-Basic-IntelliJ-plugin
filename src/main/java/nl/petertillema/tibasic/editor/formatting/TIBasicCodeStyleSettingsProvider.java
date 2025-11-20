daec0ca7e870b038094b4966bf7c8c2f096327f9
Peter Tillema <ptillema@quintor.nl>
2025-11-20 13:03:21 +0100

    Fix TIBasicCodeStyleSettingsProvider to have a language

--- src/main/java/nl/petertillema/tibasic/editor/formatting/TIBasicCodeStyleSettingsProvider.java
+++ src/main/java/nl/petertillema/tibasic/editor/formatting/TIBasicCodeStyleSettingsProvider.java
@@ -3,6 +3,7 @@ package nl.petertillema.tibasic.editor.formatting;
 import com.intellij.application.options.CodeStyleAbstractConfigurable;
 import com.intellij.application.options.CodeStyleAbstractPanel;
 import com.intellij.application.options.TabbedLanguageCodeStylePanel;
+import com.intellij.lang.Language;
 import com.intellij.openapi.util.NlsContexts;
 import com.intellij.psi.codeStyle.CodeStyleConfigurable;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
@@ -12,6 +13,11 @@ import nl.petertillema.tibasic.language.TIBasicLanguage;
 import org.jetbrains.annotations.NotNull;
 
 public final class TIBasicCodeStyleSettingsProvider extends CodeStyleSettingsProvider {
+
+    @Override
+    public @NotNull Language getLanguage() {
+        return TIBasicLanguage.INSTANCE;
+    }
 
     @Override
     public @NotNull CustomCodeStyleSettings createCustomSettings(@NotNull CodeStyleSettings settings) {
